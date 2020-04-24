f24b030210a11c63cd33d1b00963d6859bd4362e
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-04-25 04:16:56 +0530

    bug fix

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,32 +1,32 @@
 export const Navigation = () => (
-  <nav class="pt-8">
-    <div class="flex justify-between">
+  <nav className="pt-8">
+    <div className="flex justify-between">
       <a
         href="/"
-        class="transition transition-colors duration-300 hover:text-blue-800 text-blue-500 text-lg tracking-wider cursor-pointer"
+        className="transition transition-colors duration-300 hover:text-blue-800 text-blue-500 text-lg tracking-wider cursor-pointer"
       >
         Melvin George
       </a>
-      <ul class="text-blue-500 text-lg hidden md:block">
-        <li class="inline-block tracking-wider ml-6 first:ml-0">
+      <ul className="text-blue-500 text-lg hidden md:block">
+        <li className="inline-block tracking-wider ml-6 first:ml-0">
           <a
-            class="cursor-pointer transition transition-colors duration-300 hover:text-blue-900 hover:bg-gray-400 p-2 rounded-sm"
+            className="cursor-pointer transition transition-colors duration-300 hover:text-blue-900 hover:bg-gray-400 p-2 rounded-sm"
             href=""
           >
             Blog
           </a>
         </li>
-        <li class="inline-block tracking-wider ml-6 first:ml-0">
+        <li className="inline-block tracking-wider ml-6 first:ml-0">
           <a
-            class="cursor-pointer transition transition-colors duration-300 hover:text-blue-900 hover:bg-gray-400 p-2 rounded-sm"
+            className="cursor-pointer transition transition-colors duration-300 hover:text-blue-900 hover:bg-gray-400 p-2 rounded-sm"
             href=""
           >
             Projects
           </a>
         </li>
-        <li class="inline-block tracking-wider ml-6 first:ml-0">
+        <li className="inline-block tracking-wider ml-6 first:ml-0">
           <a
-            class="cursor-pointer transition transition-colors duration-300 hover:text-blue-900 hover:bg-gray-400 p-2 rounded-sm"
+            className="cursor-pointer transition transition-colors duration-300 hover:text-blue-900 hover:bg-gray-400 p-2 rounded-sm"
             href=""
           >
             About
