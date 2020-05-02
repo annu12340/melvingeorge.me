1a8a42765266d9c27ceead1f0c9cfeadeb7745d9
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-02 11:00:45 +0530

    added default seo configuration

---
+++ components/Seo/next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://melvingeorge.now.sh/",
+    site_name: "MELVIN GEORGE Blog",
+  },
+  twitter: {
+    handle: "@melvin2016_",
+    site: "@melvin2016_",
+    cardType: "summary_large_image",
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import "../styles/global.css";
+import { DefaultSeo } from "next-seo";
+import SEO from "../components/Seo/next-seo.config";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO}></DefaultSeo>
+      <Component {...pageProps} />
+    </>
+  );
 }
