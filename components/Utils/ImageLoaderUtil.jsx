c04394d6d76a05e5137a90132e6b124009d27823
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-02 10:02:36 +0530

    refactores package json for dev dependencies

--- components/Utils/ImageLoaderUtil.jsx
+++ components/Utils/ImageLoaderUtil.jsx
@@ -1,34 +1,35 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-const ImageLoaderUtil = ({ alt: alternative, src: source }) => {
+const ImageLoaderUtil = ({ alt: altText, src: source }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
   const { src, trace } = require(`../../content/assets/${source}?trace`);
 
   return (
     <p className="relative">
-      <AnimatePresence>
-        <motion.img
-          key={src + Math.random()}
-          className="w-full top-0 left-0 absolute"
-          src={src}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: imageLoaded ? 1 : 0 }}
-          alt={alternative}
-          loading="lazy"
-          transition={{ ease: "easeIn" }}
-          onLoad={() => setImageLoaded(true)}
-        />
-        <motion.img
-          key={src}
-          className="w-full"
-          src={trace}
-          alt={alternative}
-          initial={{ opacity: 1 }}
-          animate={{ opacity: imageLoaded ? 0 : 1 }}
-          transition={{ ease: "easeOut" }}
-        />
-      </AnimatePresence>
+      <a className="cursor-pointer" href={src} target="blank">
+        <AnimatePresence>
+          <motion.img
+            key={src}
+            className="w-full top-0 left-0 absolute"
+            src={src}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: imageLoaded ? 1 : 0 }}
+            alt={altText}
+            transition={{ ease: "easeIn" }}
+            onLoad={() => setImageLoaded(true)}
+          />
+          <motion.img
+            key={trace}
+            className="w-full"
+            src={trace}
+            alt={altText}
+            initial={{ opacity: 1 }}
+            animate={{ opacity: imageLoaded ? 0 : 1 }}
+            transition={{ ease: "easeIn" }}
+          />
+        </AnimatePresence>
+      </a>
     </p>
   );
 };
