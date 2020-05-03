051d88a2fbf361b29de82876f46e878fcca65fd4
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-03 14:46:41 +0530

    added profile ld+json seo format

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "../styles/global.css";
-import { DefaultSeo } from "next-seo";
+import { DefaultSeo, SocialProfileJsonLd } from "next-seo";
 import DefaultSeoConfig from "../components/Seo/default-seo.config";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...DefaultSeoConfig}></DefaultSeo>
+      <SocialProfileJsonLd
+        type="Person"
+        name="MELVIN GEORGE"
+        url="http://melvingeorge.me/"
+        sameAs={[
+          "https://www.facebook.com/alkitj10",
+          "https://github.com/melvin2016",
+          "https://www.linkedin.com/in/melvin2016/",
+          "https://twitter.com/melvin2016_",
+        ]}
+      />
       <Component {...pageProps} />
     </>
   );
