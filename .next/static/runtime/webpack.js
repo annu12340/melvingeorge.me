00aa623847553db98f97ac7358dd5f1e79bab027
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-05 19:54:07 +0530

    new content

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6aa3a1805bc148825b35";
+/******/ 	var hotCurrentHash = "df542b438ad0e08529d7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
