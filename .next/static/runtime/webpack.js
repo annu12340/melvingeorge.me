3e7c6e6b0572223b027fcecec9fea8614be9ff79
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-08 15:14:00 +0530

    changed small css modules to styles jsx and optimised index page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9253d3ddeb5ec5690c8c";
+/******/ 	var hotCurrentHash = "378f605a84d6a5432113";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
