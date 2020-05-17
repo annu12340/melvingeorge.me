b63bdf29afe1a1a700273c724dc9acfc6e4a3e4f
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-17 21:44:53 +0530

    added app shell for navigation

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { DefaultSeo, SocialProfileJsonLd } from "next-seo";
 import DefaultSeoConfig from "../default-seo.config";
 import { ActiveTabProvider } from "../context/ActiveTab";
 import "../styles/global.css";
+import Navigation from "../components/Navigation/Navigation";
 
 const App = ({ Component, pageProps }) => {
   return (
@@ -19,6 +20,7 @@ const App = ({ Component, pageProps }) => {
         ]}
       />
       <ActiveTabProvider>
+        <Navigation isIndex={false} />
         <Component {...pageProps} />
       </ActiveTabProvider>
     </>
