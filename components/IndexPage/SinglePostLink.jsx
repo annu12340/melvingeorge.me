aec33f5426f6de639830da8c8866a9eb900a66d9
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-31 23:25:44 +0530

    changed link /posts/* ==> /blog/*

--- components/IndexPage/SinglePostLink.jsx
+++ components/IndexPage/SinglePostLink.jsx
@@ -6,14 +6,14 @@ const FontAwesomeIcon = dynamic(
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const SinglePostLink = ({ title, href, description }) => (
   <div className="mb-10">
-    <Link href="/posts/[link]" as={`/posts/${href}`}>
+    <Link href="/blog/[link]" as={`/blog/${href}`}>
       <span className="text-2xl font-medium cursor-pointer duration-300 transition transition-colors  hover:bg-gray-200 hover:text-blue-600">
         {title.charAt(0).toUpperCase() + title.slice(1)}
       </span>
     </Link>
     <p className="italic mt-1 text-xl font-normal">{description}</p>
     <a href="" className="inline-block cursor-pointer text-blue-500 mt-1">
-      <Link href="/posts/[link]" as={`/posts/${href}`}>
+      <Link href="/blog/[link]" as={`/blog/${href}`}>
         <span className="font-light text-xl">
           Read <FontAwesomeIcon icon={faArrowRight} width="0"></FontAwesomeIcon>
         </span>
