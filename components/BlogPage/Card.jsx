ec882fa71abd38b2077fe17f89f185804bb41c26
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-31 23:52:00 +0530

    adding blog page

---
+++ components/BlogPage/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+const ImageLoader = dynamic(() => import("../Utils/ImageLoaderUtil"));
+
+const Card = ({ title, description, href }) => {
+  return (
+    <Link href={`/blog/${href}`}>
+      <div className="p-6 bg-white shadow-sm rounded cursor-pointer group">
+        <h1 className="text-2xl my-4 tracking-wide text-center font-medium">
+          {title}
+        </h1>
+        <ImageLoader
+          link={href}
+          alt={`Banner picture of ${title} blog`}
+          src="main.jpg"
+        />
+        <p className="tracking-wide my-4 leading-relaxed text-lg italic transition transition-colors duration-300 group-hover:text-blue-600">
+          {description}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
