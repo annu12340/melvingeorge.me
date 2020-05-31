113aabbaa8070ce156f86bfec987f478e56d48c5
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-05-31 23:19:30 +0530

    changed href of back button to blog page instead of index page

--- components/Utils/GoBackButton.jsx
+++ components/Utils/GoBackButton.jsx
@@ -6,7 +6,7 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 const GoBackButton = () => (
-  <Link href="/">
+  <Link href="/blog">
     <a className="fixed inline-block px-3 py-2 shadow-md bg-gray-700 rounded-full text-gray-100 mr-5 mb-5 bottom-0 right-0 opacity-75 duration-200 hover:opacity-100">
       <FontAwesomeIcon icon={faArrowLeft} size="1x" width="0"></FontAwesomeIcon>
       <span className="hidden md:inline"> Go Back</span>
