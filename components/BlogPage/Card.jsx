10fad47d43579569d6824705a18ab70c36351463
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-06-01 01:40:29 +0530

    bug fix

--- components/BlogPage/Card.jsx
+++ components/BlogPage/Card.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import dynamic from "next/dynamic";
-const ImageLoader = dynamic(() => import("../Utils/ImageLoaderUtil"));
+import ImageLoader from "../Utils/ImageLoaderUtil";
 
 const Card = ({ title, description, href }) => {
   return (
