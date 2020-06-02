51680b72e919b737c3f0db8a5b23131ece79b58f
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-06-02 20:21:48 +0530

    seo fix for twitter

--- components/PostsPage/BlogSEO.jsx
+++ components/PostsPage/BlogSEO.jsx
@@ -30,8 +30,8 @@ const BlogSEO = ({ metadata }) => (
         ],
       }}
       twitter={{
-        handle: "@melvin2016_",
-        site: "@melvin2016_",
+        handle: "@developer_369",
+        site: "@developer_369",
         cardType: "summary_large_image",
       }}
     />
