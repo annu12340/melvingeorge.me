3ffb0b4227eda6d760373dbc549db3e926f1bfdb
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-06-02 06:41:11 +0530

    bug fix and added sharer

--- components/IndexPage/Main.jsx
+++ components/IndexPage/Main.jsx
@@ -3,7 +3,7 @@ import SinglePostLink from "./SinglePostLink";
 
 const Main = ({ linksData }) => (
   <main className="z-0 relative container max-w-screen-md mx-auto px-4 md:px-20 md:-mt-8 lg:-mt-12 py-10 mb-12 md:shadow-lg md:rounded-lg bg-white">
-    <h1 className="text-2xl mb-10 font-semibold text-gray-800">Blog</h1>
+    <h1 className="text-2xl mb-10 font-semibold text-gray-800">Recent blogs</h1>
     {linksData && linksData.length <= 0
       ? null
       : linksData.map((linkData) => (
