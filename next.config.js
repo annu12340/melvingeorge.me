edfb7e2287e25044ffef484102bbfa5c18c5bd9f
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-06-27 10:01:23 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 const nextOffline = require("next-offline");
+
 module.exports = withPlugins(
   [
-    [
-      optimizedImages,
-      {
-        optimizeImagesInDev: true,
-        imageTrace: {
-          color: "#4299e1",
-          alphaMax: 2,
-        },
-      },
-    ],
     [
       nextOffline,
       {
         workboxOpts: {
-          // Do not precache images
-          exclude: [/\.(?:png|jpg|jpeg|svg|webp)$/],
+          exclude: [
+            "react-loadable-manifest.json",
+            "build-manifest.json",
+            /\.map$/,
+            /\.(?:png|jpg|jpeg|svg|webp)$/,
+          ],
+          modifyURLPrefix: {
+            "static/": "_next/static/",
+            "public/": "_next/public/",
+          },
+          inlineWorkboxRuntime: true,
           // Ignore all URL parameters.
           ignoreURLParametersMatching: [/.*/],
           dontCacheBustURLsMatching: /.*/,
@@ -41,10 +41,20 @@ module.exports = withPlugins(
         },
       },
     ],
+    [
+      optimizedImages,
+      {
+        optimizeImagesInDev: true,
+        imageTrace: {
+          color: "#4299e1",
+          alphaMax: 2,
+        },
+      },
+    ],
   ],
   {
     devIndicators: {
       autoPrerender: false,
     },
-  },
+  }
 );
