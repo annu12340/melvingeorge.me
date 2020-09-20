63089eeabdde01741ec5d0fe9e09ea0dc4d49393
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-09-20 19:04:12 +0530

    added mdx support

--- components/PostsPage/HighlightedMarkdown.jsx
+++ components/PostsPage/HighlightedMarkdown.jsx
@@ -1,43 +1,24 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import dynamic from "next/dynamic";
-import Markdown from "markdown-to-jsx";
-const hljs = require("highlight.js/lib/core");
-hljs.registerLanguage(
-  "javascript",
-  require("highlight.js/lib/languages/javascript"),
+const ImageLoaderUtil = dynamic(() =>
+  import("../../content/components/ImageLoaderUtil")
 );
-hljs.registerLanguage("bash", require("highlight.js/lib/languages/bash"));
-hljs.registerLanguage("json", require("highlight.js/lib/languages/json"));
-hljs.registerLanguage("css", require("highlight.js/lib/languages/css"));
-hljs.registerLanguage(
-  "html",
-  require("highlight.js/lib/languages/vbscript-html"),
-);
-const ImageLoaderUtil = dynamic(() => import("../Utils/ImageLoaderUtil"));
-export function HighlightedMarkdown({ children, metadata }) {
+import hydrate from "next-mdx-remote/hydrate";
+import customComponents from "../../content/components/index";
+
+export function HighlightedMarkdown({ children, metadata: { link } }) {
   const rootRef = useRef();
 
-  useEffect(() => {
-    rootRef.current.querySelectorAll("pre code").forEach((block) => {
-      hljs.highlightBlock(block);
-    });
-  }, [children]);
+  const content = hydrate(children, {
+    components: {
+      ...customComponents,
+      img: (attrs) => <ImageLoaderUtil link={link} {...attrs} />,
+    },
+  });
 
   return (
     <div ref={rootRef} className="blog">
-      <Markdown
-        options={{
-          overrides: {
-            img: {
-              component: (props) => {
-                return <ImageLoaderUtil {...props} link={metadata.link} />;
-              },
-            },
-          },
-        }}
-      >
-        {children}
-      </Markdown>
+      {content}
     </div>
   );
 }
