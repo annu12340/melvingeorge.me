d0e2c662f7e1301cf7812ffe609afa763dae4bea
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-09-20 19:04:32 +0530

    relocated imageloaderutil

---
+++ content/components/ImageLoaderUtil.jsx
@@ -0,0 +1,16 @@
+import "lazysizes";
+const ImageLoaderUtil = ({ alt: altText, src: source, link }) => {
+  const { trace } = require(`../assets/${link}/${source}?trace`);
+  const src = require(`../assets/${link}/${source}?webp`);
+  return (
+    <img
+      alt={altText}
+      src={trace}
+      data-srcset={src}
+      className="lazyload blur-up"
+      data-sizes="auto"
+    />
+  );
+};
+
+export default ImageLoaderUtil;
