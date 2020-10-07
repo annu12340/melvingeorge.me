dd1eced5858c50af3aee930a1d9f6de74054ea3c
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-10-07 21:29:09 +0530

    added some shadow ab testing

--- content/components/ImageLoaderUtil.jsx
+++ content/components/ImageLoaderUtil.jsx
@@ -7,7 +7,7 @@ const ImageLoaderUtil = ({ alt: altText, src: source, link }) => {
       alt={altText}
       src={trace}
       data-srcset={src}
-      className="lazyload blur-up"
+      className="lazyload blur-up shadow"
       data-sizes="auto"
     />
   );
